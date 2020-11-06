48f3e5c29968a72ca7a7e79c225dffd492927ae9
James Todd <jtodd@pioneeringevolution.com>
2020-11-05 18:48:41 -0600

    updates

--- app/Console/Commands/CheckOutages.php
+++ app/Console/Commands/CheckOutages.php
@@ -16,10 +16,10 @@ class CheckOutages extends Command
         $time = time();
         foreach (Host::where('status', 'up')->get() as $host) {
             $minutes = round(($time - $host->last_ping) / 60, 0);
-            if ($minutes > 1) {
+            if ($minutes > 15) {
                 $lastDateString = date("Y-m-d h:i a", $host->last_ping);
                 $hostName = $host->hostname;
-                $message = "Host: {$hostName} is down\nLast known: " . $lastDateString . "\n{$minutes} ago";
+                $message = "Host: {$hostName} is down\nLast known: " . $lastDateString . "\n{$minutes} minutes ago";
                 $service = new SendSmsService();
                 $service->sendMessage(env('NOTIFICATION_CELL'), $message);
                 $this->line($message);
