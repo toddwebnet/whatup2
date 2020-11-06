ac2bc0e3663f2bfdbf944fa6efc1864cd799424e
James Todd <jtodd@pioneeringevolution.com>
2020-11-06 07:18:20 -0600

    updates

--- app/Models/Host.php
+++ app/Models/Host.php
@@ -6,4 +6,12 @@ use Illuminate\Database\Eloquent\Model;
 class Host extends Model
 {
     protected $fillable = ['hostname', 'last_ping', 'status'];
+
+    public function toArray()
+    {
+        return [
+                'datetime' => date("Y-m-d i:h a", $this->last_ping)
+            ] +
+            parent::toArray();
+    }
 }
